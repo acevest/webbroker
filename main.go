d978363f4861af9df90ae871fcdc24c9c4ef40ff
acevest <zhaoyanbai@126.com>
2020-09-16 22:23:46 +0800

    支持修改一个tcp连接的多次http请求的请求头

--- main.go
+++ main.go
@@ -138,111 +138,26 @@ func handleHTTPClient(clientConn net.Conn) {
 		log.Printf("nil client")
 		return
 	}
+	var err error
 
 	defer clientConn.Close()
 
 	clientAddr := clientConn.RemoteAddr().String()
 	log.Printf("accept new connection from %v", clientAddr)
 
-	// 存储解析出的虚拟主机名
-	var host string
-
-	// 要解析http头，就要读出数据，而这部分数据还必需发送到虚拟主机
-	// 因此还需要将它们缓存起来
-	const maxBufferSize = 4096
-	buffer := bytes.NewBuffer(make([]byte, 0, maxBufferSize))
-
-	reader := bufio.NewReader(io.TeeReader(clientConn, buffer))
-
-	for {
-		// 对于占用了很大缓存依旧没有分析到Host字段的请求
-		// 直接认定为异常请求
-		if buffer.Len() > maxBufferSize {
-			log.Printf("err: invalid http header, the header is too large")
-			return
-		}
-
-		data, _, err := reader.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				//log.Printf(">>>>>>>>>> END <<<<<<")
-				break
-			}
-		}
-		line := string(data)
-		if len(line) < 6 {
-			continue
-		}
-
-		prefix := strings.ToLower(line[:5])
-		if prefix != "host:" {
-			continue
-		}
-
-		fields := strings.Split(line, ":")
-		if len(fields) != 2 && len(fields) != 3 {
-			log.Printf("err: invalid host field in http header: %v", line)
-			return
-		}
-
-		host = fields[1]
-
-		// 顺便插入客户端的真实ip:port信息到http头里
-		clientIP, _, _ := net.SplitHostPort(clientAddr)
-		buffer.WriteString("X-Real-IP: " + clientIP + "\r\n")
-
-		break
-	}
-
-	if len(host) == 0 {
-		log.Printf("err: empty hostname")
-		return
-	}
-
-	// 找出虚拟主机地址
-	hostAddr, err := getVirtualHTTPHostAddr(host)
-	if err != nil {
-		log.Printf("err: unsupport host %v, err: %v", host, err)
-		return
-	}
-
-	// 连接虚拟主机
-	hostConn, err := net.Dial("tcp", hostAddr)
-	if err != nil {
-		log.Printf("err: connect to virtual host %v[%v] failed: %v", host, hostAddr, err)
-		return
-	}
-
-	defer hostConn.Close()
-
-	// 插入broker信息
-	localIP, _, _ := net.SplitHostPort(hostConn.LocalAddr().String())
-	buffer.WriteString("X-Forwarded-For: " + localIP + "\r\n")
-
 	// client的链接可能单方面关闭
 	// 为了避免另一方向的数据拷贝中断
 	// 将他们放进两个goroutine并等待
 	var wg sync.WaitGroup
 	wg.Add(2)
-
-	// 再将余下的数据传递虚拟主机
-	go func() {
-		defer wg.Done()
-		defer hostConn.(*net.TCPConn).CloseWrite()
-		defer func() {
-			if c, ok := clientConn.(*net.TCPConn); ok {
-				c.CloseRead()
-			}
-		}()
-
-		_, err = io.Copy(hostConn, io.MultiReader(buffer, reader))
-		log.Printf("copy from client %v to host %v. local %v err %v\n", clientAddr, hostAddr, hostConn.LocalAddr().String(), err)
-	}()
+	var chanConn = make(chan net.Conn, 1)
 
 	// 将虚拟主机返回的数据传递给前端
 	go func() {
+		hostConn := <-chanConn
 		defer wg.Done()
 		defer hostConn.(*net.TCPConn).CloseRead()
+
 		defer func() {
 			switch c := clientConn.(type) {
 			case *net.TCPConn:
@@ -254,7 +169,84 @@ func handleHTTPClient(clientConn net.Conn) {
 		}()
 
 		_, err = io.Copy(clientConn, hostConn)
+		hostAddr := hostConn.LocalAddr().String()
 		log.Printf("copy from host %v to client %v. local %v err %v\n", hostAddr, clientAddr, hostConn.LocalAddr().String(), err)
+	}()
+
+	// 再将余下的数据传递虚拟主机
+	go func() {
+		// 存储解析出的虚拟主机名
+		var host string
+
+		reader := bufio.NewReader(clientConn)
+
+		defer wg.Done()
+		var hostConn net.Conn
+		defer func() {
+			if hostConn != nil {
+				hostConn.(*net.TCPConn).CloseWrite()
+			} else {
+				log.Printf("host conn is nil")
+			}
+		}()
+		defer func() {
+			if c, ok := clientConn.(*net.TCPConn); ok {
+				c.CloseRead()
+			}
+		}()
+
+		for {
+			req, err := http.ReadRequest(reader)
+			if err != nil {
+				if err != io.EOF {
+					log.Printf("read request, err: %v", err)
+					return
+				}
+
+				break
+			}
+
+			const maxBufferSize = 4096
+			buffer := bytes.NewBuffer(make([]byte, 0, maxBufferSize))
+			if len(host) == 0 {
+				host = req.Host
+				log.Printf("host:   %v", host)
+
+				// 找出虚拟主机地址
+				hostAddr, err := getVirtualHTTPHostAddr(host)
+				if err != nil {
+					log.Printf("err: unsupport host %v, err: %v", host, err)
+					return
+				}
+
+				// 连接虚拟主机
+				hostConn, err = net.Dial("tcp", hostAddr)
+				if err != nil {
+					log.Printf("err: connect to virtual host %v[%v] failed: %v", host, hostAddr, err)
+					return
+				}
+
+				chanConn <- hostConn
+			}
+
+			// 插入客户端的真实ip信息到http头里
+			clientIP, _, _ := net.SplitHostPort(clientAddr)
+			req.Header.Add("X-Real-IP", clientIP)
+
+			// 插入broker信息
+			localIP, _, _ := net.SplitHostPort(hostConn.LocalAddr().String())
+			prevXForwardFor := req.Header.Get("X-Forwarded-For")
+			if len(prevXForwardFor) != 0 {
+				prevXForwardFor = "," + prevXForwardFor
+			}
+			req.Header.Add("X-Forwarded-For", localIP+prevXForwardFor)
+
+			req.Write(buffer)
+
+			_, err = io.Copy(hostConn, buffer)
+			log.Printf("copy from client %v to host %v. local %v err %v\n", clientAddr, "", hostConn.LocalAddr().String(), err)
+		}
+
 	}()
 
 	wg.Wait()
